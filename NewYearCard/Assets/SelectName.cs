6a088c91f3723539e147dc35b47520a844cb592f
mrrrlisin <33547742+mrrrlisin@users.noreply.github.com>
2017-12-23 23:18:47 +0300

    Add files via upload

---
+++ NewYearCard/Assets/SelectName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class SelectName : MonoBehaviour
+{
+    public VideoClip[] clips;
+    public VideoPlayer player;
+
+    public void Select(int number)
+    {
+        player.clip = clips[number];
+        gameObject.SetActive(false);
+    }
+
+}
